3a752a3be95d553702287b52af0dd8f9637877c8
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-04-15 09:50:21 +0300

    add: course and 404 components and scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CoursesComponent } from './courses/courses.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: 'courses',
+    component: CoursesComponent,
+    data: { title: 'Courses' }
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'courses'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
