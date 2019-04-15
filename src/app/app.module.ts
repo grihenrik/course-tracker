0ec0a88d7345fa63285d75365c90ecd3d7a591af
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-04-15 10:40:23 +0300

    add: retrieve course data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CourseComponent } from './course/course.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './services/data.service';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    CoursesComponent,
-    PageNotFoundComponent,
-    CourseComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, CoursesComponent, PageNotFoundComponent, CourseComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
